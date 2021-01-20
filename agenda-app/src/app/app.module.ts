234105b07ac9c6212857555732b5cf1f4b66724c
Victor <victorcarnevale@yahoo.com.br>
2021-01-20 00:35:04 -0300

    Forms

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ContatoService } from './contato.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ContatoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ agenda-app/src/app/contato/contato.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContatoService } from '../contato.service';
+import { Contato } from './contato';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+export class ContatoComponent implements OnInit {
+
+  formulario: FormGroup;
+
+  constructor(
+    private service: ContatoService,
+    private  fb: FormBuilder
+    ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['',[ Validators.email,Validators.required]]
+    })
+  }
+
+  submit() {
+    console.log(this.formulario.value);
+    const isValid = this.formulario.valid;
+  }
+
+}
